34a43dbc276f15866d276e77d52c5c4b08f152f9
dkacp <damkac4@st.amu.edu.pl>
2023-04-09 21:32:27 +0200

    next

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from sklearn.datasets import make_circles
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from torch import nn
+from helper_functions import plot_predictions, plot_decision_boundary
+import requests
+from pathlib import Path
+
 
 # Make 1000 samples
 n_samples = 1000
@@ -39,3 +44,90 @@ X_train, X_test, y_train, y_test = train_test_split(X,
                                                     y,
                                                     test_size=0.2, # 20% test, 80% train
                                                     random_state=42) # make the random split reproducible
+# Set device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Build model with non-linear activation function
+class CircleModelV2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features=2, out_features=10)
+        self.layer_2 = nn.Linear(in_features=10, out_features=10)
+        self.layer_3 = nn.Linear(in_features=10, out_features=1)
+        self.relu = nn.ReLU() # <- add in ReLU activation function
+
+
+    def forward(self, x):
+      # Intersperse the ReLU activation function between layers
+       return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))
+
+model_3 = CircleModelV2().to(device)
+
+# Create a loss function
+# loss_fn = nn.BCELoss() # BCELoss = no sigmoid built-in
+loss_fn = nn.BCEWithLogitsLoss()
+
+# Create an optimizer function
+optimizer = torch.optim.SGD(model_3.parameters(), lr=0.1)
+
+
+# Calculate accuracy (a classification metric)
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()  # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+epochs = 2000
+
+# Put all data on target device
+X_train, y_train = X_train.to(device), y_train.to(device)
+X_test, y_test = X_test.to(device), y_test.to(device)
+
+for epoch in range(epochs):
+
+    ### Training
+    model_3.train()
+
+    # 1. Forward pass (The raw outputs of our model are often referred to as logits.)
+    y_logits = model_3(X_train).squeeze() # squeeze to remove extra `1` dimension
+    y_pred = torch.round(torch.sigmoid(y_logits))  # logits -> prediction probabilities -> prediction labels
+
+    # 2. Calculate loss and accuracy
+    loss = loss_fn(y_logits, y_train)  # BCEWithLogitsLoss calculates loss using logits
+    acc = accuracy_fn(y_true=y_train,
+                      y_pred=y_pred)
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Loss backward
+    loss.backward()
+
+    # 5. Optimizer step
+    optimizer.step()
+
+    ### Testing
+    model_3.eval()
+    with torch.inference_mode():
+        # 1. Forward pass
+        test_logits = model_3(X_test).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))  # logits -> prediction probabilities -> prediction labels
+        # 2. Calcuate loss and accuracy
+        test_loss = loss_fn(test_logits, y_test)
+        test_acc = accuracy_fn(y_true=y_test,
+                               y_pred=test_pred)
+
+    # Print out what's happening
+    if epoch % 100 == 0:
+        print(
+            f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Accuracy: {test_acc:.2f}%")
+
+
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Train")
+plot_decision_boundary(model_3, X_train, y_train)
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model_3, X_test, y_test)
+plt.show()
